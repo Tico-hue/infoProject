fd117befdca22df3d6282290e0db2caeb491b232
Tico <lautarotico@gmail.com>
2020-10-01 13:33:34 -0300

    .

---
+++ infoProject/infoProject/infoProject/apps/perfil/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from apps.usuarios.models import Usuario
+from PIL import Image
+from enum import IntEnum
+
+class Rubro(IntEnum):
+    OTROS = 1
+    INDUMENTARIA = 2
+    CERVECERIA = 3
+    COSMETICO = 4
+    SERVICIOS = 5
+
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+    @classmethod
+    def crearRubro(cls):
+        pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    image = models.ImageField(default = 'default-user-image.jpg', upload_to='profile_pics',blank=True)
+    aboutus = models.TextField(default="Sobre nosotros...")
+    instagram = models.CharField(max_length=30, default="Instagram...")
+    twitter = models.CharField(max_length=30, default="@Twitter...")
+    telefono = models.BigIntegerField(default=0)
+    rubro = models.IntegerField(choices=Rubro.choices(), default=Rubro.OTROS)
+    
+    
+
+    def __str__(self):
+        return f'Perfil de {self.user.username}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        print(img)
+        if img.height > 300 or img.width >300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
---
+++ infoProject/infoProject/infoProject/apps/usuarios/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from .models import Usuario
+from ..productos.models import Producto
+
+def register(request):
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, f'Tu cuenta ha sido creada, ya podes ingresar')
+			return redirect('login')
+	else:
+		form = UserRegisterForm()
+	return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+	if request.method == 'POST':
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+		if p_form.is_valid():
+			p_form.save()
+			messages.success(request, f'Tu cuenta ha sido actualizada')
+			return  get_user_profile(request,request.user)
+
+	else:
+		p_form = ProfileUpdateForm(instance = request.user.profile)
+
+	context = {
+		'p_form': p_form
+	}
+	return render(request, 'usuarios/profile.html',context)
+
+
+def get_user_profile(request, username):
+	user = Usuario.objects.get(username=username)
+	context = {}
+	todos = Producto.objects.filter(usuario_id__id__icontains=user.id)
+	context['productos'] = todos
+	return render(request, 'usuarios/user_profile.html', {'profile_user':user,'productos':todos})
